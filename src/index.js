360eb450eec0563ed06b8ff48050b0530c4f68df
juliawalkowiak <walkowiak.jula@gmail.com>
2023-12-11 19:39:04 +0100

    added responsive icon

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ function refreshWeather(response) {
   let pressureElement = document.querySelector("#pressure");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
+  let iconElement = document.querySelector("#icon");
+  
+  
+  //console.log(response);
+  
+  
+  iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
