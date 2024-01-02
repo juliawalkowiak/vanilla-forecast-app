0ad31d23cda41fd0d9dbdf2e4f0e65f967113fb8
juliawalkowiak <walkowiak.jula@gmail.com>
2024-01-02 21:44:44 +0100

    fixed time function

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ function refreshWeather(response) {
 }
 
 function formatDate(date) {
-  let hours = date.getHours();
   let minutes = date.getMinutes();
+  let hours = date.getHours();
   let days = [
     "Sunday",
     "Monday",
@@ -40,12 +40,14 @@ function formatDate(date) {
   ];
 
   let day = days[date.getDay()];
+  
+  if (minutes < 10) {
+  minutes =`0${minutes}`;
+    } 
+    
   return `${day}, ${hours}:${minutes}`;
 
-  if (minutes < 10) {
-minutes = `0${minutes}`;
-  }
-}
+};
 
 function searchCity(city) {
   let apiKey = "1174f301a46105cf0a5e5e9e991bdodt";
