d1d750cb0f1ab548cfdbbd3270ca5307188764a5
juliawalkowiak <walkowiak.jula@gmail.com>
2024-01-03 00:29:19 +0100

    added initial forecast

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ function refreshWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
-  
-  
+
   //console.log(response);
-  
-  
+
   iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -40,14 +38,13 @@ function formatDate(date) {
   ];
 
   let day = days[date.getDay()];
-  
-  if (minutes < 10) {
-  minutes =`0${minutes}`;
-    } 
-    
-  return `${day}, ${hours}:${minutes}`;
 
-};
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "1174f301a46105cf0a5e5e9e991bdodt";
@@ -61,7 +58,28 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+  
+  days.forEach(function (day) {
+    forecastHtml = forecastHtml +` <div class="weather-forecast">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperature"> 
+    <span class="weather-forecast-temp-max">18°</span>
+    <span class="weather-forecast-temp-min">12°</span>
+    </div>
+    </div>`;
+  });
+  
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Warsaw");
+displayForecast();
